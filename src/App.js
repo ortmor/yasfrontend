b898f879b41d8566deeb6016efc2198f30a89102
Abhirami V V <abhiramivv77@gmail.com>
2024-05-17 00:32:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ const App = () => {
       .catch((error) => {
         console.log(error);
       })
-      .finally(() => setCheckAuth(true));
+      .finally(() => setCheckAuth(false));
   }, [refresh, dispatch]);
 
   return (
